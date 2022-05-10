0e95f0e6b9142c3f09e831e65fc89f3c8c5988ce
Basheerbachi <basheerbachi066@gmail.com>
2022-05-10 20:44:09 +0530

    first commit

---
+++ src/components/Register.js
@@ -0,0 +1,22 @@
+// import React from 'react'
+
+// function Guest(){
+//     return(
+//         <>
+//             <h1>Welcome Abdul Basheer</h1>
+//             <button>Logout !</button>
+//         </>
+//     )
+// }
+
+
+
+// const Register = (props) =>{
+//     console.log(props);
+//     const loggedIn = props.loggedIn;
+
+//     if(loggedIn){
+//         return <Welcome />
+//     }
+//     return <Guest />
+// }
---
+++ src/components/preview.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+const Preview = () {
+    return(
+        <>
+            <h1>Styling using functional and classComponent</h1>
+
+            <button className="but1">lkijh</button>
+        </>
+    ) 
+    
+    }
